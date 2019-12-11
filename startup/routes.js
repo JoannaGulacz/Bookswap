204e682aff5fc59867c339271053661a6c300fc4
Andrzej <eviljajo@gmail.com>
2019-12-11 21:35:05 +0100

    reviews.json updated with id. Routes corrected

--- startup/routes.js
+++ startup/routes.js
@@ -5,6 +5,7 @@ const booksRouter = require('../routes/books');
 const bookcasesRouter = require('../routes/bookcases');
 const usersRouter = require('../routes/users');
 const categoriesRouter = require('../routes/categories');
+const reviewsRouter = require('../routes/review');
 
 module.exports = app => {
     app.use('/api/tests', testRouter);
@@ -14,4 +15,5 @@ module.exports = app => {
     app.use('/api/bookcases', bookcasesRouter);
     app.use('/api/users', usersRouter);
     app.use('/api/categories', categoriesRouter);
+    app.use('/api/reviews', reviewsRouter);
 };
