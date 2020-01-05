b731f3867aa9ef65026729bfd3a4423fa636b3c0
Joanna Gulacz <57046956+JoannaGulacz@users.noreply.github.com>
2020-01-05 19:39:22 +0100

    searching books added

--- client/src/pages/Swap.jsx
+++ client/src/pages/Swap.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import BooksToSwap from '../components/BooksToSwap';
+import axios from '../utils/axios';
+import List from '../components/List';
+export default class Swap extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: [],
+        };
+    }
 
-const Swap = () => {
-    return (
-        <>
-            <BooksToSwap />
-        </>
-    );
-};
+    updateBooks = BookList => {
+        this.setState({ books: BookList });
+    };
 
-export default Swap;
+    async componentDidMount() {
+        await axios
+            .get('bookcases/swaps')
+            .then(response => {
+                this.setState({ books: response.data.data });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <BooksToSwap updateBooks={this.updateBooks} />
+                <List list={this.state.books} />
+            </div>
+        );
+    }
+}
