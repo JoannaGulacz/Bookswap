8ac52ebeed696982114ba7333c062e043ac677b3
Adam Malczewski <adamalcz@gmail.com>
2020-01-07 14:11:31 +0100

    Warning fixed

--- client/src/components/logo/Logo.jsx
+++ client/src/components/logo/Logo.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Logo = props => {
-    return <h1 className="text-center">{props.title}</h1>;
+    return (
+        <h1 className="text-center">
+            <i className="fas fa-book-medical mx-2"></i>
+            {props.title}
+            <i className="fas fa-address-book mx-2"></i>
+        </h1>
+    );
 };
 
 export default Logo;
