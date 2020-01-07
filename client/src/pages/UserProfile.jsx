2bd135abbd8e0fbba5749b5192d746f310a980d2
Adam Malczewski <adamalcz@gmail.com>
2020-01-07 04:34:47 +0100

    MDBContainer component moved to root page

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MDBCol, MDBContainer, MDBRow } from 'mdbreact';
+import { MDBCol, MDBRow } from 'mdbreact';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import UserInfo from '../components/user_profile/UserInfo';
@@ -10,23 +10,21 @@ import PasswordMsg from '../components/user_profile/PasswordMsg';
 
 const UserProfile = props => {
     return (
-        <MDBContainer>
-            <Router>
-                <MDBRow>
-                    <MDBCol md="6">
-                        <UserInfo userName={props.userName} role={props.role} email={props.email} />
-                        <PasswordButton />
-                        <PasswordMsg />
-                    </MDBCol>
-                    <MDBCol md="6">
-                        <Switch>
-                            <Route path="/users/me" exact component={UserAvatar} />
-                            <Route path="/users/me/password" render={props => <PasswordForm {...props} />} />
-                        </Switch>
-                    </MDBCol>
-                </MDBRow>
-            </Router>
-        </MDBContainer>
+        <Router>
+            <MDBRow>
+                <MDBCol md="6">
+                    <UserInfo userName={props.userName} role={props.role} email={props.email} />
+                    <PasswordButton />
+                    <PasswordMsg />
+                </MDBCol>
+                <MDBCol md="6">
+                    <Switch>
+                        <Route path="/users/me" exact component={UserAvatar} />
+                        <Route path="/users/me/password" render={props => <PasswordForm {...props} />} />
+                    </Switch>
+                </MDBCol>
+            </MDBRow>
+        </Router>
     );
 };
 
