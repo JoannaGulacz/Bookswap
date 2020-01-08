0a39477ab338d9aa8f9ac563bb95e467ff89b776
Dominika Korbas <you@example.comdominique065@gmail.com>
2020-01-08 15:23:52 +0100

    Book fix

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import Root from './pages/root';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 serviceWorker.unregister();
